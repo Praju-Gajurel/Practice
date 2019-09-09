66787622d3d91f05b2e1d2437710b0a7ef41c3be
Praju-Gajurel <54385226+Praju-Gajurel@users.noreply.github.com>
2019-09-08 19:48:33 -0500

    3D Game

---
+++ Assets/SliderCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider MySlider;
+    public Text Praju;
+
+
+    void Update()
+    {
+        Praju.text = MySlider.value.ToString("F2");
+        
+
+
+
+    }
+}
